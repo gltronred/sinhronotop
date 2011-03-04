d5bd2405c2d2c2899fc278154451b212c9854135
ttzt <perlinmi@gmail.com>
2011-03-04 21:08:49 +0100

    load teams 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,7 +62,7 @@ File.open(File.join(Rails.root, 'db', "teams.csv"), 'r') do |file|
     atributes = line.split(';')
     rating_id, team_name, city_name = atributes[0], atributes[1], atributes[2]
     #city = SeedTasks.create_city(city_name
-    SeedTasks.create_team(rating_id, team_name, city)
+    SeedTasks.create_team(rating_id, team_name, city_name)
   end
 end
 
