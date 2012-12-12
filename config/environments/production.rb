93fc1a2579451ea450cd3462c77319e783a5bcbc
ttzt <perlinmi@gmail.com>
2012-12-12 02:32:54 +0100

    add logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,3 +29,10 @@ config.action_view.cache_template_loading            = true
 
 config.action_mailer.default_url_options = { :host => 'sinhronotop.heroku.com' }
 config.action_mailer.perform_deliveries = true
+
+Sinhronotop::Application.configure do
+
+  # add this line
+  config.logger = Logger.new(STDOUT)
+
+end
