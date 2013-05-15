41710d9b7a047e8ca55dce87976714b13b36a110
Mansur Ziatdinov <gltronred@gmail.com>
2013-05-16 00:10:57 +0400

    Removed deprecated iconv from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,6 @@ RAILS_GEM_VERSION = '2.3.18' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'iconv'
 class RackRailsCookieHeaderHack
   def initialize(app)
     @app = app
